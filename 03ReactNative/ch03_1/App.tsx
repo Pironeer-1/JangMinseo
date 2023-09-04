2962a6482aebaf4c6903a65ffdf87f7c0a461678
minseo jang <mingdo.dev@gmail.com>
2023-09-05 01:28:32 +0900

    03ReactNative: MD2Colors 클래스 수정

--- 03ReactNative/ch03_1/App.tsx
+++ 03ReactNative/ch03_1/App.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
 import { MD2Colors } from 'react-native-paper' // 수정
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
-console.log(Colors.blue500)
+console.log(MD2Colors.blue500)
 export default function App() {
   return (
     <SafeAreaView style={[styles.SafeAreaView]}>
@@ -15,6 +14,6 @@ export default function App() {
 }
 const styles = StyleSheet.create({
   SafeAreaView: {flex: 1, alignItems: 'center', justifyContent: 'center'
-    , backgroundColor: Colors.blue500},
-  text: {fontSize: 20, color: Colors.blue200}
+    , backgroundColor: MD2Colors.blue500},
+  text: {fontSize: 20, color: MD2Colors.blue200}
 })

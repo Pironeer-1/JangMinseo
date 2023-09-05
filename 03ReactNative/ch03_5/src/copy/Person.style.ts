92e2b4adc19fad34d0664cd68af2f6de17212d0c
minseo jang <mingdo.dev@gmail.com>
2023-09-05 18:43:43 +0900

    03ReactNative: 재사용할 수 있는 컴포넌트 만들기

--- 03ReactNative/ch03_5/src/copy/Person.style.ts
+++ 03ReactNative/ch03_5/src/copy/Person.style.ts
@@ -1,4 +1,19 @@
 import { StyleSheet } from "react-native";
 import { MD2Colors } from "react-native-paper";
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+    view: {backgroundColor: MD2Colors.lime100, padding: 5},
+    leftview: {padding: 5},
+    rightview: {flex: 1, padding: 5, marginRight: 10},
+    avatar: {width: 50, height: 50, borderRadius: 25},
+    nameEmailView: {flexDirection: 'row', alignItems: 'center'},
+    name: {marginRight: 5, fontSize: 22, fontWeight: '500'},
+    email: {textDecorationLine: 'underline',
+        color: MD2Colors.blue500, textDecorationColor: MD2Colors.blue500},
+    dateView: {flexDirection: 'row', justifyContent: 'space-between', padding: 3, marginTop: 5},
+    text: {fontSize: 16},
+    image: {width: '100%', height: 150},
+    countsView: {flexDirection: 'row', padding: 3, justifyContent: 'space-around'},
+    touchableIcon: {flexDirection: 'row', padding: 5, alignItems: 'center'},
+    iconText: {color: MD2Colors.deepPurple500, marginLeft: 3}
+})

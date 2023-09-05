9113925b906bde6a756df91b9da681557ce10ca1
minseo jang <mingdo.dev@gmail.com>
2023-09-05 15:02:34 +0900

    03ReactNative: 컴포넌트 배치 관련 스타일 속성 탐구하기

--- 03ReactNative/ch03_4/src/screens/Content.tsx
+++ 03ReactNative/ch03_4/src/screens/Content.tsx
@@ -1,29 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import { MD2Colors } from 'react-native-paper'
+import { StyleSheet, View, Image, ScrollView } from 'react-native'
+// import { MD2Colors } from 'react-native-paper'
 import * as D from '../data'
 
-const title = 'Content'
+const avatars = D.makeArray(200).map((notused) => D.randomAvatarUrl())
 export default function Content() {
-    return (
-        <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
-            <View style={[{flex: 0, backgroundColor: MD2Colors.red500}]}>
-                <Text>flex: 0</Text>
-            </View>
-            <View style={[{flex: 1, backgroundColor: MD2Colors.green500}]}>
-                <Text>flex: 1</Text>
-            </View>
-            <View style={[{flex: 2, backgroundColor: MD2Colors.purple500}]}>
-                <Text>flex: 2</Text>
-            </View>
+    const children = avatars.map((avatarUrl, index) => (
+        <View key={index.toString()} style={styles.avatarView}>
+            <Image style={styles.avatar} source={{uri: avatarUrl}} />
         </View>
+    ))
+    return (
+        <ScrollView contentContainerStyle={[styles.view]}>{children}</ScrollView>
     )
 }
 const styles = StyleSheet.create({
     view: {
-        flex: 1, padding: 5,
-        backgroundColor: MD2Colors.lightBlue100
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        // flex: 1;
+        padding: 5
     },
-    text: {fontSize: 20, color: 'white'}
+    text: {fontSize: 20},
+    avatarView: {padding: 3},
+    avatar: {width: 50, height: 50, borderRadius: 25}
 })

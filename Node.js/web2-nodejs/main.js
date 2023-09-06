6cf19ed18f4625d940acaf3b79fbb00a7e6b3977
minseo jang <mingdo.dev@gmail.com>
2023-09-07 02:11:30 +0900

    Node.js: 10 URL로 입력된 값 사용하기

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -1,15 +1,18 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // Node.js한테 url이라는 모듈이 필요해!!!!!!!!!!!
+
 var app = http.createServer(function(request, response) {
-    var url = request.url;
-    if (request.url == '/') {
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if (_url == '/') {
+        _url = '/index.html';
     }
-    if (request.url == '/favicon.ico') {
+    if (_url == '/favicon.ico') {
         return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url)); //웹 서버가 웹 브라우저가 요청한 파일을 읽어서 응답
+    response.end(queryData.id); //웹 서버가 웹 브라우저가 요청한 파일을 읽어서 응답
 });
-app.listen(80);
+app.listen(3000);

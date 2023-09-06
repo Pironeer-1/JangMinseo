fc933184d26954c0125ad8720cd674ecc0a7921a
minseo jang <mingdo.dev@gmail.com>
2023-09-06 23:47:52 +0900

    Node.js: 05 웹 서버 만들기

---
+++ Node.js/web2-nodejs/main.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request, response) {
+    var url = request.url;
+    if (request.url == '/') {
+        url = '/index.html';
+    }
+    if (request.url == '/favicon.ico') {
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    console.log(__dirname + url);
+    response.end(fs.readFileSync(__dirname + url)); //웹 서버가 웹 브라우저가 요청한 파일을 읽어서 응답
+});
+app.listen(3000);

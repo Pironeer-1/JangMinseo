52fe36c4d23be69f9964f3124974542e3c9899e2
minseo jang <mingdo.dev@gmail.com>
2023-09-07 20:43:03 +0900

    Node.js: 18 자바스크립트 콘솔에서 입력받기

--- Node.js/web2-nodejs/syntax/conditional.js
+++ Node.js/web2-nodejs/syntax/conditional.js
@@ -1,6 +1,8 @@
+var args = process.argv; // 프로그램 실행 시 함께 넘어온 입력값. 명령 뒤에 입력값 명시해야 함!!
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if (false) {
+if (args[2] == 1) {
     console.log('C1');
 
 }

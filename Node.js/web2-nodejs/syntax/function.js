facfa47208b0f917a88643ab0ef543f2377975e4
minseo jang <mingdo.dev@gmail.com>
2023-09-07 21:53:59 +0900

    Node.js: 25 자바스크립트 함수

---
+++ Node.js/web2-nodejs/syntax/function.js
@@ -0,0 +1,12 @@
+f123();
+console.log('아앙아아아ㅏ아아');
+console.log('hi');
+f123();
+f123();
+console.log('만수무강하세요');
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+}
---
+++ Node.js/web2-nodejs/syntax/function2.js
@@ -0,0 +1,10 @@
+// console.log(Math.round(1.6));
+// console.log(Math.round(1.4));
+
+function sum(first, second) {
+    // console.log(first + second);
+    return first + second; // 반환 및 함수 !!!!!!종료!!!!!!
+}
+
+sum(2, 4); // first, second: 매개변수 / 2,4: 인자
+console.log(sum('hi', ' chicken'));

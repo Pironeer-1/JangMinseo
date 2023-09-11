2cf6d09130bb6820e9404536bafe14a5bfc1e969
minseo jang <mingdo.dev@gmail.com>
2023-09-12 02:24:54 +0900

    docs: 불필요한 모듈 제거

--- InstagramClone/insta-app/App.js
+++ InstagramClone/insta-app/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import SignedInStack from './navigation';
-import NewPostScreen from "./screens/NewPostScreen";
 
 export default function App() {
   return <SignedInStack/>

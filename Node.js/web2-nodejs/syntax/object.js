1bd822d57daa93de2606b07e49d68f34ebb19461
minseo jang <mingdo.dev@gmail.com>
2023-09-14 22:12:58 +0900

    Node.js: 39 자바스크립트 객체의 형식

---
+++ Node.js/web2-nodejs/syntax/object.js
@@ -0,0 +1,10 @@
+// var members = ['hehe', 'hihi', 'hoho'];
+// console.log(members[1]);
+
+var roles = {
+    'programmer': 'hehe',
+    'designer' : 'hihi',
+    'manager' : 'hoho'
+}
+console.log(roles.programmer);
+console.log(roles['designer']);

0d31534e73ecf6fdd6debb21848304780c6deb7c
minseo jang <mingdo.dev@gmail.com>
2023-09-14 18:18:52 +0900

    Node.js: 36 App 글 수정 (수정된 내용 저장)

--- Node.js/web2-nodejs/main.js
+++ Node.js/web2-nodejs/main.js
@@ -93,7 +93,7 @@ var app = http.createServer(function(request, response) {
                 response.end('');
             })
         }); // 약속
-    } else if (pathname === `/update`) {
+    } else if (pathname === '/update') {
         fs.readdir('./data', function(err, filelist) {
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
                 var title = queryData.id;
@@ -110,6 +110,23 @@ var app = http.createServer(function(request, response) {
                     </form>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
                 response.writeHead(200);
                 response.end(template);
+            });
+        });
+    } else if (pathname === '/update_process'){
+        var body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+                response.writeHead(302, {Location: `/?id=${title}`});
+                response.end();
+                })
             });
         });
     } else {
